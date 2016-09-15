c68bef2dc927c0682fb93b4e977cba957646ebbc
NikitaCartes <nikich98@yandex.ru>
2016-09-15 23:34:21 +0300

    Update P1.pas

--- P1.pas
+++ P1.pas
@@ -1,11 +1,12 @@
 Program p1;
-    var i1, i2:integer;
+    var i1, i2, i3, i4, i5, n:integer;
 	var mas: array [0..11, 0..41] of char;
 	var c: char;
 begin
+	n:=10;
 	assign(input, 'C:\Users\nikic\Desktop\in.txt');
 	reset(input);
-	for i1:=1 to 10 do
+	for i1:=1 to n do
 	begin
 		for i2:=1 to 40 do
 		begin
@@ -39,5 +40,53 @@ begin
 			end;
 		end;
 	end;
-	readln;
+	writeln('');
+	for i1:=1 to n-1 do
+	begin
+		for i3:=i1+1 to n do
+		begin
+			for i2:=1 to 40 do
+			begin
+				if (mas[i1, i2] <> mas[i3, i2]) then
+				begin
+					break;
+				end
+				else if ((mas[i1, i2] = ' ') and (mas[i3, i2] = ' ')) then
+				begin
+					for i4:=i3 to n-1 do
+					begin
+						for i5:=1 to 40 do
+						begin
+							if (Ord(mas[i4, i5]) <> 12) then
+							begin
+								mas[i4, i5]:=mas[i4+1, i5];
+							end
+							else
+							begin
+								mas[i4, i5]:=mas[i4+1, i5];
+								Continue;
+							end;
+						end;
+					end;
+					n:=n-1;
+				end
+			end;
+		end;
+	end;
+	for i1:=1 to n do
+	begin
+		for i2:=1 to 40 do
+		begin
+			read(c);
+			if (Ord(c) <> 12) then
+			begin
+				write(mas[i1, i2]);
+			end
+			else
+			begin
+				write(mas[i1, i2]);
+				Continue;
+			end;
+		end;
+	end;
 end.
