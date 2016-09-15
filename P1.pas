8e1545d1425197e53e242f126ad26b5d75d6e639
NikitaCartes <nikich98@yandex.ru>
2016-09-16 00:33:59 +0300

    Update P1.pas

--- P1.pas
+++ P1.pas
@@ -2,6 +2,7 @@ Program p1;
     var i1, i2, i3, i4, i5, n:integer;
 	var mas: array [0..11, 0..41] of char;
 	var c: char;
+	label metka;
 begin
 	n:=10;
 	assign(input, 'C:\Users\nikic\Desktop\in.txt');
@@ -41,38 +42,8 @@ begin
 		end;
 	end;
 	writeln('');
-	for i1:=1 to n-1 do
-	begin
-		for i3:=i1+1 to n do
-		begin
-			for i2:=1 to 40 do
-			begin
-				if (mas[i1, i2] <> mas[i3, i2]) then
-				begin
-					break;
-				end
-				else if ((mas[i1, i2] = ' ') and (mas[i3, i2] = ' ')) then
-				begin
-					for i4:=i3 to n-1 do
-					begin
-						for i5:=1 to 40 do
-						begin
-							if (Ord(mas[i4, i5]) <> 12) then
-							begin
-								mas[i4, i5]:=mas[i4+1, i5];
-							end
-							else
-							begin
-								mas[i4, i5]:=mas[i4+1, i5];
-								Continue;
-							end;
-						end;
-					end;
-					n:=n-1;
-				end
-			end;
-		end;
-	end;
+	writeln('');
+	
 	for i1:=1 to n do
 	begin
 		for i2:=1 to 40 do
