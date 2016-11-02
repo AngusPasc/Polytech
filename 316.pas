2eb81cafaf8890b968e721e240fc86664e8678cf
NikitaCartes <nikich98@yandex.ru>
2016-11-03 02:00:06 +0300

    Update 316.pas

--- 316.pas
+++ 316.pas
@@ -23,49 +23,84 @@ Program program1;
 			writeln(tec^.name);
 			last:=tec;
 		end;
+		writeln('');
 	end;
+
 	procedure Sort(aaa : gg);
-	var fio, ez, fio1, ez1, fio2, ez2: gg;
+	var  fio, ez, b1, b2, b3, b4, b5: gg;
 	begin
-		fio:=aaa;
-		ez:=aaa;
-		writeln('');
-		writeln('');
+		fio:=aaa^.next;
 		while fio^.next <> nil do
 		begin
-			ez:=fio^.next;
+			ez:= fio^.next;
 			while ez <> nil do
 			begin
-				if (fio^.name > ez^.name) then
+				if fio^.name > ez^.name then
 				begin
-					fio1:=aaa;
-					While fio1^.next <> fio do
-					Begin
-						fio1:= fio1^.next;
+					if fio^.next = ez then
+					begin
+						b1:=aaa;
+						while b1^.next <> fio do
+							b1:=b1^.next;
+						b2:=b1;
+						
+						b1^.next:=ez;
+						fio^.next:=ez^.next;
+						ez^.next:=fio;
+						
+						fio:=b2^.next;
+						ez:=fio^.next;
+					end
+					else if fio^.next^.next = ez then
+					begin
+						b1:=aaa;
+						while b1^.next <> fio do
+							b1:=b1^.next;
+						b2:=b1;
+						b3:=fio^.next;
+						
+						b1^.next:=ez;
+						fio^.next:=ez^.next;
+						ez^.next:=b3;
+						b3^.next:=fio;
+						
+						fio:=b2^.next;
+						ez:=fio^.next^.next;
+					end
+					else
+					begin
+						b1:=aaa;
+						b2:=fio;
+						while b1^.next <> fio do
+							b1:=b1^.next;
+						while b2^.next <> ez do
+							b2:=b2^.next;
+						b3:=b1;
+						b4:=b2;
+						b5:=ez^.next;
+						
+						ez^.next:=fio^.next;
+						b1^.next:=ez;
+						fio^.next:=b5;
+						b2^.next:=fio;
+						
+						fio:=b3^.next;
+						ez:=b4^.next;
 					end;
-					ez1:=aaa;
-					While ez1^.next <> ez do
-					Begin
-						ez1:= ez1^.next;
-					end;
-					fio2:=fio^.next;
-					ez2:=ez^.next;
-					fio1^.next:=ez;
-					ez1^.next:=fio;
-					fio^.next:=ez2;
-					ez^.next:=fio2;
 				end;
-			ez:= ez^.next;
+				ez:=ez^.next;
 			end;
-		fio:=fio^.next;
+			fio:=fio^.next;
 		end;
 	end;
 	
-	
-	procedure MakeList(first : gg);
+	procedure MakeList(var first : gg);
 	var ez, fio: gg;
 	begin
-		fio:=first;
+		new(fio);
+		fio^.next:=first;
+		first:=fio;
+		fio:=first^.next;
 		ez:=fio;
 		readln(fio^.name);
 		fio^.next:=nil;
@@ -86,6 +121,8 @@ begin
 	MakeList(fio);
 	assign(output, 'C:\Users\nikic\Desktop\OUT.TXT');
 	rewrite(output);
+	FileWrite(fio^.next);
+	{close(output);}
 	Sort(fio);
-	FileWrite(fio);
+	FileWrite(fio^.next);
 end.
